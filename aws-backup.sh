81bada8954e730883cd383b8bf2e83cefba2a813
Renat Sapegin <sapegin.renat@gmail.com>
2018-04-13 09:29:56 +0300

    Update aws-backup.sh

--- aws-backup.sh
+++ aws-backup.sh
@@ -1,65 +1,116 @@
 #!/bin/bash
+export PATH=$PATH:/usr/local/bin/:/usr/bin
+source /opt/aws/config.conf
 
-# EC2_HOME 
+# Safety feature: exit script if error is returned, or if variables not set.
+# Exit if a pipeline results in an error.
+set -ue
+set -o pipefail
 
-# JAVA_HOME
+## Automatic EBS Volume Snapshot Creation & Clean-Up Script
+#
+# PURPOSE: This Bash script can be used to take automatic snapshots of your Linux EC2 instance. Script process:
+# - Determine the instance ID of the EC2 server on which the script runs
+# - Gather a list of all volume IDs attached to that instance
+# - Take a snapshot of each attached volume
+# - The script will then delete all associated snapshots taken by the script that are older than 28 days
+#
+# DISCLAIMER: This script deletes snapshots (though only the ones that it creates). 
+# Make sure that you understand how the script works. No responsibility accepted in event of accidental data loss.
+#
 
-# Regions reference
 
-# You can find your instance ID at AWS Manage Console
+## Variable Declartions ##
 
-instanceID="YOUR-INSTANCE-ID"
+# Get Instance Details
+instance_id=$(wget -q -O- http://169.254.169.254/latest/meta-data/instance-id)
+region=$(wget -q -O- http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/\([1-9]\).$/\1/g')
 
-# Your prefer AMI Name prefix
+# Set Logging Options
+logfile="/var/log/ebs-snapshot.log"
+logfile_max_lines="5000"
 
-amiNamePrefix="AMI_"
+# How many days do you wish to retain backups for? Default: 28 days
+retention_days="28"
+retention_date_in_seconds=$(date +%s --date "$retention_days days ago")
 
-# Your prefer AMI Description
-amiDescription="Backup"
 
-# If you want to keep 7 days AMI backups, please set routine true otherwise set it false
-routine=true
+## Function Declarations ##
 
-# Variable for routine is true
-weekday=$(date +%a)
+# Function: Setup logfile and redirect stdout/stderr.
+log_setup() {
+    # Check if logfile exists and is writable.
+    ( [ -e "$logfile" ] || touch "$logfile" ) && [ ! -w "$logfile" ] && echo "ERROR: Cannot write to $logfile. Check permissions or sudo access." && exit 1
 
-if [ $routine = true ]; then
-    # Setup AMI Name
-    amiName=$snapshotIDs$weekday
+    tmplog=$(tail -n $logfile_max_lines $logfile 2>/dev/null) && echo "${tmplog}" > $logfile
+    exec > >(tee -a $logfile)
+    exec 2>&1
+}
 
-    # Get AMI ID
-    amiIDs=$(ec2-describe-images --region $region | grep 'ami-[a-z0-9]' | grep "$amiName" |cut -f 2)
+# Function: Log an event.
+log() {
+    echo "[$(date +"%Y-%m-%d"+"%T")]: $*"
+}
 
-    # Get Snapshot ID
-    if [[ ! -z $amiIDs ]]; then
-        snapshotIDs=$(ec2-describe-snapshots --region $region | grep $amiIDs | cut -f 2)
-    fi
-else
-    # Setup AMI Name
-    amiName=$amiNamePrefix
+# Function: Confirm that the AWS CLI and related tools are installed.
+prerequisite_check() {
+	for prerequisite in aws wget; do
+		hash $prerequisite &> /dev/null
+		if [[ $? == 1 ]]; then
+			echo "In order to use this script, the executable \"$prerequisite\" must be installed." 1>&2; exit 70
+		fi
+	done
+}
 
-    # Get AMI ID
-    amiIDs=$(ec2-describe-images --region $region | grep 'ami-[a-z0-9]' | cut -f 2)
+# Function: Snapshot all volumes attached to this instance.
+snapshot_volumes() {
+	for volume_id in $volume_list; do
+		log "Volume ID is $volume_id"
 
-    # Get Snapshot ID
-    if [[ ! -z $amiIDs ]]; then
-        snapshotIDs=$(ec2-describe-snapshots --region $region | cut -f 2)
-    fi
-fi
+		# Get the attched device name to add to the description so we can easily tell which volume this is.
+		device_name=$(aws ec2 describe-volumes --region $region --output=text --volume-ids $volume_id --query 'Volumes[0].{Devices:Attachments[0].Device}')
 
-if [[ ! -z $amiIDs ]]; then
-    # Deregister AMI
-    for amiID in $amiIDs
-    do
-        ec2-deregister --region $region $amiID
-    done
+		# Take a snapshot of the current volume, and capture the resulting snapshot ID
+		snapshot_description="$volume_id-$device_name-backup-$(date +%Y-%m-%d)"
 
-    # Delete snapshot
-    for snapshotID in $snapshotIDs
-    do
-        ec2-delete-snapshot --region $region $snapshotID
-    done
-fi
+		snapshot_id=$(aws ec2 create-snapshot --region $region --output=text --description $snapshot_description --volume-id $volume_id --query SnapshotId)
+		log "New snapshot is $snapshot_id"
+	 
+		# Add a "CreatedBy:AutomatedBackup" tag to the resulting snapshot.
+		# Why? Because we only want to purge snapshots taken by the script later, and not delete snapshots manually taken.
+		aws ec2 create-tags --region $region --resource $snapshot_id --tags Key=CreatedBy,Value=AutomatedBackup
+	done
+}
 
-# Create AMI
-ec2-create-image $instanceID --region $region --name "$amiName" -d "$amiDescription" --no-reboot
+# Function: Cleanup all snapshots associated with this instance that are older than $retention_days
+cleanup_snapshots() {
+	for volume_id in $volume_list; do
+		snapshot_list=$(aws ec2 describe-snapshots --region $region --output=text --filters "Name=volume-id,Values=$volume_id" "Name=tag:CreatedBy,Values=AutomatedBackup" --query Snapshots[].SnapshotId)
+		for snapshot in $snapshot_list; do
+			log "Checking $snapshot..."
+			# Check age of snapshot
+			snapshot_date=$(aws ec2 describe-snapshots --region $region --output=text --snapshot-ids $snapshot --query Snapshots[].StartTime | awk -F "T" '{printf "%s\n", $1}')
+			snapshot_date_in_seconds=$(date "--date=$snapshot_date" +%s)
+			snapshot_description=$(aws ec2 describe-snapshots --snapshot-id $snapshot --region $region --query Snapshots[].Description)
+
+			if (( $snapshot_date_in_seconds <= $retention_date_in_seconds )); then
+				log "DELETING snapshot $snapshot. Description: $snapshot_description ..."
+				aws ec2 delete-snapshot --region $region --snapshot-id $snapshot
+			else
+				log "Not deleting snapshot $snapshot. Description: $snapshot_description ..."
+			fi
+		done
+	done
+}	
+
+
+## SCRIPT COMMANDS ##
+
+log_setup
+prerequisite_check
+
+# Grab all volume IDs attached to this instance whith special tags
+volume_list=$(aws ec2 describe-volumes --region $region --query Volumes[].VolumeId --filters "Name=tag:$TAG_KEY,Values=$TAG_VALUE" --output text)
+
+snapshot_volumes
+cleanup_snapshots
